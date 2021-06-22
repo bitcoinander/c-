e94986bc6d019e8e7ff403d54d55f842db124d3b
oAnder/ <32654298+bitcoinander@users.noreply.github.com>
2021-06-22 17:54:20 -0300

    Update media.cpp

--- media.cpp
+++ media.cpp
@@ -4,42 +4,33 @@ using namespace std;
 
 int main()
 {
- int n1, n2, media=0;
-//pedir para o usuário colocar a primeira nota 
- printf("Digite a Nota 1: ");
- scanf("%d", &n1);
-//pedir para o usuário colocar a segunda nota 
- printf("Digite a Nota 2: ");
- scanf("%d", &n2);
- 
-//calculando média...
- media = (n1 + n2)/2;
+ float n1, n2,n3,n4, media=0;
+ char operacao;
 
-//se for maior ou igual a 7,'aluno aprovado', se for menor 'aluno reprovado'.
- if (media >= 7)
- {
-     printf("Aluno Aprovado");
- }
- 
- if (media ==4){
-     
-     printf("Aluno em exame");
- }
- 
-  if (media ==5){
-     
-     printf("Aluno em exame");
- }
- 
-  if (media ==6){
-     
-     printf("Aluno em exame");
- }
- 
-  if (media <4){
-     
-     printf("Aluno Reprovado");
- }
-    
-    return 0;
+cout <<"CALCULANDO MÉDIA EM C++" << endl;
+cout <<"DIGITE [M]PARA CONTINUAR:_";
+cin >> operacao;
+cout << endl;
+
+switch (operacao)
+{
+    case 'm':   
+ cout << endl <<"Digite a primeira nota:";
+ cin >> n1;
+ cout << endl <<"Digite a segunda nota:";
+ cin >> n2;
+ cout << endl <<"Digite a terceira nota:";
+ cin >> n3;
+ cout << endl <<"Digite a quarta nota:";
+ cin >> n4;
+ media = (n1+n2+n3+n4)/4;
+ cout << "A sua média é:"<<media <<endl <<endl;
+ cout << endl;
+ break;
+
+default: 
+cout <<"operação invalida..." <<endl;
+}
+return 0;
+
 }
